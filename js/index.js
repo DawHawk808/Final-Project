c49f411d1fd9b37a51e3650082c7187d5e9dd9eb
JeffTheReff <dahearn23@punahou.edu>
2016-07-07 13:47:42 -1000

    JS FROM CHARLES

--- js/index.js
+++ js/index.js
@@ -3,22 +3,34 @@
 //Dean the very awesome, amazing, and handsome bean Does Javascript
 //Dawson helps everybody else
 
-var Player =
+var attackPower;
 
-var Monster = [
-	"Lobster",
-	"Slime",
-	"Armored Man",
-	"Zombie",
-	"Slender Man"]
+var bow = [0,0,0,0,10,20,50]
 
 
-var Music = newAudio
-
-play(true)
-stop = false
-loop(true)
-
-function StartGame(){
+var nextMonster = function() {
+	document.getElementById('monster').innerHTML = "Slime, Lobster, Slenderman"
 }
 
+var attack = function(){
+	if (monsterHP <= 0){
+		console.log("Your Monster's Innards Became Their Outards By Your Attack!");o
+		document.getElementById("monster").innerHTML = "";
+		document.getElementById("result").innerHTML = "Your Monster's Innards Became Their Outards By Your Attack!";
+
+	} else {
+		attackPower = Math.floor(Math.random()*14);
+		monsterHP = monsterHP - attackPower;
+		console.log("Your attack power is "+ attackPower);
+		console.log("Monster HP is "+monsterHP);
+		document.getElementById("result").innerHTML = "Monster HP: "+monsterHP;
+	}
+}
+
+var monsterCheck = function(){
+	if (monsterHP <= 0){
+		console.log("Your Monster is dead");
+		document.getElementById("monster").innerHTML = "";
+		document.getElementById("result").innerHTML = "Your Monster is Dead!";o
+	}
+}
