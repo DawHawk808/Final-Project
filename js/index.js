618985040138166b0e09738ae82605996c5c2235
DawHawk808 <dhsoe23@punahou.edu>
2016-07-11 12:54:28 -1000

    needs work

--- js/index.js
+++ js/index.js
@@ -16,7 +16,7 @@ var attackPower;
 
 var bow = [0,0,0,0,10,20,50]
 
-var monsterHP = 300;
+var monsterHP = Math.random(Math.floor)*301;
 
 function nextMonster() {
 	var Badguys = Math.floor(Math.random()*Monsters.length);
