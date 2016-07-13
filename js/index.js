1f9068903880519bc198844b0dbff835e26eb3ee
GreenMachine808 <mherrera23@punahou.edu>
2016-07-13 13:27:13 -1000

    changed everything

--- js/index.js
+++ js/index.js
@@ -80,12 +80,3 @@ break;
 function docReady(){
 window.addEventListener('keydown', moveSelection);
 };
-
-
-function arrowanimation() {
-	var arrow = document.getElementById("arrow")
-	if (player.pos > arrow.pos) {
-	element.style.top = parseInt(element.style.top) + 15 + 'px';
-
-	}
-}
